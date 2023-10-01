4a18b642661fa0697212037151076e6ac4a2ad6e
hasanovanar <74391045+hasanovanar@users.noreply.github.com>
2023-10-01 21:39:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+// console.log(22);
+
+// let ad = "Ehmed";
+// let familiya = "Veliyev";
+// let tamAd = ad + " " + familiya;
+// console.log(tamAd);
+
+// console.log("salam");
+// console.log(5 / 0);
+// console.log("ikinci salam");
+
+// let dogru = false;
+// dogru = String(dogru);
+// console.log(typeof dogru);
+
+// let yanlis = false;
+// yanlis = +yanlis;
+// console.log(yanlis);
+
+// let metn = "false";
+// metn = Boolean(metn);
+// console.log(typeof metn);
+// console.log(metn);
+
+// let komanda1 = 'REAL';
+// let komanda2 = 'Barcelona';
+// let 3komanda = 'Deportivo' // icaze yoxdur
+// let if = 'alma' // icaze yoxdur
+// let for = 'armud' // icaze yoxdur
+// let menim adim = 'Anar' // icaze yoxdur
+// let menimAdim = 'Anar' // duzgundur
+// let olkeninPaytaxti = 'Baki' // camelCase - 1-ci sozden sonra diger sozle boyuk yazilir
+// let myCountry = 'Azerbaijan'
+// let myAge = 45
+// let myCountryCapital = 'Baku'
+// let mySchoolNumber = 158
+// // MyCountry - Boyuk herfle JS-de klasslari, konstruktorlari adlandirirlar
+// let _komanda = 'Qarabag' //icaze var
+
+let money = 100;
+money = 100 + 200;
+console.log(money);
+
+const birthYear = 1986;
+// birthYear = 1989; // const-la beyan olunmus deyisenlere yeni qiymet verile bilmez
+// console.log(birthYear); // xeta verecek
+
+// console.log(25);
+
+let ders; // beyan edirem
+ders = "Fizika"; // qiymet verirem
+// let ders = "Kimya"; // eyniadli deyisenleri 2-ci defe beyan etmek olmaz
+
+let game = "Football"; // beyan edirem ve qiymet verirem.
+game = "Handbol"; // deyisirem
+let imtahanQiymeti = 5; // beyan edirem ve qiymet verirem.
+let imtahanKimya; // sadece beyan
+imtahanKimya = 4; // qiymet verirem
+
+// const bilet; // sadece beyana icaze yoxdur
+const bilet = "kino"; // qiymet verilenden sonra deyisile bilmez
+const product = "tort"; // deyisene setir qiymeti verende dirnaq icerisinde olmalidir
+// product = "sampun"; // const-la beyan olundugundan bu xeta verecek
+// console.log(product);
+
+let alma = 2;
+let uzum = 3;
+let cemPul = alma + uzum;
+console.log(cemPul);
+
+let saftaliKq = 2;
+let saftaliQiymet = 1.5;
+let kartofKq = 2;
+let kartofQiymet = 1.9;
+
+let yekunPul = saftaliKq * saftaliQiymet + kartofKq * kartofQiymet;
+console.log(yekunPul);
+
+let bananQiymet = 2.8;
+let bananKq = 1;
+let mebleg = bananQiymet * bananKq;
+console.log(mebleg);
+
+bananQiymet = bananQiymet + 0.2;
+bananQiymet = 3.5;
+bananQiymet = bananQiymet - 0.3;
+console.log(bananQiymet);
+// console.log(mebleg);
+
+let seyahet = "Almaniya";
+console.log(seyahet);
+seyahet = "Fransa";
+console.log(seyahet);
+
+let myName = "Ehmed";
+let mySurname = "Cavadov";
+let myFullName = myName + " " + mySurname;
+console.log(myFullName);
+
+const menimAdim = "Veli";
+const menimFamiliyam = "Settarov";
+const menimTamAdim = menimAdim + " " + menimFamiliyam;
+console.log(menimTamAdim);
