8bdd9b25219eafc875d1621c07597cdd585afddd
hasanovanar <74391045+hasanovanar@users.noreply.github.com>
2024-09-10 14:16:53 +0400

    Add files via upload

---
+++ ders13-tap.js
@@ -0,0 +1,86 @@
+// ############################### 1 #####################################
+
+let time = 5;
+
+// Bele bir kod yazmaq lazimdir. Eger time === 10 olarsa, brauzer xeberdarliq edir.
+// "10 minutes is left till end of meeting".
+
+// ############################### 2 #####################################
+
+// Bu tapsiriqi etmek ucun 45-2 videonu ve ders3-tip.js faylini tekrarlamaq lazimdir.
+// https://github.com/hasanovanar/js-ders/blob/main/ders3-tip.js
+
+// Asagidaki deyisenlerin tipini xeberdarliqda gostermek lazimdir
+
+let text = "hello"; // numune - bu string gostermelidir
+
+let price = 25;
+
+let hasPassport = false;
+
+let error = null;
+
+// Asagidaki deyisenlerin tipi bele cumlede xeberdarliq kimi gosterilmeilidir/
+// "Type of [deyisen] is "
+
+let city; // Numune - "Type of city is undefined"
+
+let hasPaid = false;
+
+// ############################### 3 #####################################
+
+// Brauzer bele bir sual vermelidir - What is your age?
+// Cavabi alandan sonra bele bir xeberdarliq vermelidir - You are [daxil olunan] years old
+
+// Meselen 15 yazilsa - You are 15 years old.
+
+// Cavab bosh olsa o zaman xeberdarliq bele olmalidir - "You did not enter your age"
+
+// ############################### 4 #####################################
+
+// Brauzer bele bir sual vermelidir - Do you have driving license? (confirm)
+// Eger cavab true olsa, o zaman bele bir xeberdarliq olmalidir
+// "You can drive car"
+// Cavab false olsa "You cannot drive a car"
+
+// ############################### 5 #####################################
+
+// Bu tapsiriqi etmek ucun 46-1 ve 46-2 videonu ve ders3-tip.js faylini tekrarlamaq lazimdir.
+
+// Asagidaki deyisenlri true-false-a cevirib (Boolean) yeni deyisen yarat ve adina bool elave et
+
+// Numune
+
+let color = "red";
+let boolColor = Boolean(color);
+
+// bunlari ozun yaz
+
+let length = null;
+
+let width = 5;
+
+let height = "";
+
+let speed = 20;
+
+let result = -15;
+
+let quality = "good";
+
+let temperature = 0;
+
+let number = NaN;
+
+let cost;
+
+// Bundan sonra bele kod yazmaq lazimdir
+
+// eger true/false-a cevrilmis qiymet true olsa - xeberdarliq - "... is truthy"
+// else "desisen adi falsy"
+
+// Numune
+let color2 = "red";
+let boolColor2 = Boolean(color);
+
+// Brauzer bele xeberdarliq vemelidir - red is truthy
