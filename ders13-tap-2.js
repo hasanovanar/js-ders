6f5f128326a7835cf2e5fb667615ca992a7b22f1
hasanovanar <74391045+hasanovanar@users.noreply.github.com>
2024-09-18 14:44:58 +0400

    Add files via upload

---
+++ ders13-tap-2.js
@@ -0,0 +1,55 @@
+let hasTicket = false;
+
+// OR ve AND operatorlarindan istifade ederek xeberdarliq ver:
+
+// 1. "You can enter the cinema" - eger hasTicket true olarsa
+// 2. "You can not enter the cinema" - eger hasTicket false olarsa
+
+// Yuxaridaki tapsiriqlari if operatoru ile hell et
+
+// ########################################################//
+
+let isOpen = true;
+
+// OR ve AND operatorlarindan istifade ederek xeberdarliq ver:
+
+// 1.  "Shop is open" - eger isOpen true olarsa
+// 2. "Shop is closed" - eger isOpen false olarsa
+
+// Yuxaridaki tapsiriqlari if operatoru ile hell et
+
+// ########################################################//
+
+// Asagidaki deyisenin qiymetini confirm vasitesi ile daxil et
+
+let hasPassport; // Sual "Do you have a passport?"
+
+// OR ve AND operatorlarindan istifade ederek xeberdarliq ver:
+
+// 1. "You can travel" - eger hasPassport true olarsa
+// 2. "You can not travel" - eger hasPassport false olarsa
+
+// Yuxaridaki tapsiriqlari if operatoru ile hell et
+
+// ########################################################//
+
+let examScore = 550;
+
+// OR ve AND operatorlarindan istifade ederek xeberdarliq ver:
+
+// 1. "You passed the exam" - eger examScore 500-den boyukdurse
+// 2. "You did not pass the exam" - eger examScore 500-den kicik ve beraberdirse
+
+// Yuxaridaki tapsiriqlari if operatoru ile hell et
+
+// ########################################################//
+
+// Asagidaki deyisenin qiymetini prompt vasitesi ile daxil et
+let computerPrice; // Sual "How much does the computer cost?"
+
+// OR ve AND operatorlarindan istifade ederek xeberdarliq ver:
+
+// 1. "You cannot buy the computer" - eger computerPrice 2000-den boyukdurse
+// 2. "You can buy the computer" - eger computerPrice 2000-den kicik ve beraberdirse
+
+// Yuxaridaki tapsiriqlari if operatoru ile hell et
